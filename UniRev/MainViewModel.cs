7fabe8cbb7ae893d29ecb6d2009a42f6267dbe8c
Bejenari Marian <kerego.aka.nillan@gmail.com>
2016-07-22 14:18:13 +0300

    add test project and a simple test

---
+++ UniRev/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniRev
+{
+	public class MainViewModel
+	{
+
+		public int Sum(int a, int b)
+		{
+			return a + b;
+		}
+
+
+	}
+}
